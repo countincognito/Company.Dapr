eb8b8110b75942d3bfbb11ded31ada42310002ee
countincognito <countincognito@outlook.com>
2023-11-25 19:21:53 +0000

    First working example of Elsa Workflow.

---
+++ src/Company.Utility.Workflow.Service/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Company.Utility.Workflow.Service.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        // requires using Microsoft.Extensions.Configuration;
+        public readonly IConfiguration Configuration;
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ src/Company.Utility.Workflow.Service/Program.cs
@@ -0,0 +1,69 @@
+using Elsa;
+using Elsa.Persistence.EntityFramework.Core.Extensions;
+using Elsa.Persistence.EntityFramework.Sqlite;
+
+namespace Company.Utility.Workflow.Service
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var elsaSection = builder.Configuration.GetSection("Elsa");
+
+            // Elsa services.
+            builder.Services
+                .AddElsa(elsa => elsa
+                    .UseEntityFrameworkPersistence(ef => ef.UseSqlite())
+                    .AddConsoleActivities()
+                    .AddHttpActivities(elsaSection.GetSection("Server").Bind)
+                    .AddEmailActivities(elsaSection.GetSection("Smtp").Bind)
+                    .AddQuartzTemporalActivities()
+                    .AddJavaScriptActivities()
+                    .AddWorkflowsFrom<Startup>()
+                );
+
+            // Elsa API endpoints.
+            builder.Services.AddElsaApiEndpoints();
+            builder.Services.AddElsaSwagger();
+
+            // For Dashboard.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseHttpActivities();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+
+
+
+            // Elsa API Endpoints are implemented as regular ASP.NET Core API controllers.
+            app.MapControllers();
+
+            // // For Dashboard.
+            // app.MapFallbackToPage("/_Host");
+
+
+
+            app.Run();
+        }
+    }
+}
